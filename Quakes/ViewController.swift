8821cedd1c31d333a04147c283e94e08051b1a63
Adrian Bolinger <adrian.bolinger@me.com>
2019-10-24 21:01:46 -0400

    Initial Commit

---
+++ Quakes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quakes
+//
+//  Created by Adrian Bolinger on 10/24/19.
+//  Copyright © 2019 Adrian Bolinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
