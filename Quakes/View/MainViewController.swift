d5760cdb5dfcc1ad2574443a12191cccc09f5b3a
Adrian Bolinger <adrian.bolinger@me.com>
2020-01-09 20:01:28 -0500

    Update tableview

--- Quakes/View/MainViewController.swift
+++ Quakes/View/MainViewController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+//class QuakesDataSource: UITableViewDiffableDataSource<AlertLevel, Earthquake> {
+//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return "hello world"
+//    }
+//}
+
 class MainViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
@@ -35,6 +41,7 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = dataSource
+        tableView.delegate = self
         
         let queryParams = USGSQueryParams(minMag: 5,
                                           maxMag: nil,
@@ -57,17 +64,38 @@ class MainViewController: UIViewController {
     }
 }
 
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.snapshot().numberOfItems
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseID, for: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return dataSource.snapshot().sectionIdentifiers[section].rawValue.capitalized
+    }
+}
+
+extension MainViewController: UITableViewDelegate {
+    
+}
+
 extension MainViewController {
     func makeDataSource() -> UITableViewDiffableDataSource<AlertLevel, Earthquake> {
-        let reuseIdentifier = reuseID
-        
         return UITableViewDiffableDataSource(tableView: tableView) { tableView, indexPath, earthquake in
-            let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+            let cell = tableView.dequeueReusableCell(withIdentifier: self.reuseID, for: indexPath)
             cell.textLabel?.text = earthquake.properties.title
             cell.detailTextLabel?.text = earthquake.properties.detail
             
             return cell
         }
-        
     }
 }
